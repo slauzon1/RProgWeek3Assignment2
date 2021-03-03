7a764bf3fe12d04ec8a3e2e93288f6eeb0c887e9
slauzon1 <35336188+slauzon1@users.noreply.github.com>
2021-03-03 07:10:40 -0500

    Update ProgAssign2Week3.R

--- ProgAssign2Week3.R
+++ ProgAssign2Week3.R
@@ -1,4 +1,7 @@
+## Coursera R Programming Week 3: Assignment 2 Lexical Scoping
+
 ##Function makeCacheMatrix March 2, 2021 
+## This function creates a matrix that can have its inverse cached
 makeCacheMatrix <- function(x = matrix()) 
 {
   inv <- NULL
@@ -14,6 +17,8 @@ makeCacheMatrix <- function(x = matrix())
 }
 
 ##Function cacheSolve March 2, 2021 
+## Will creates the inverse of the matrix from function makeCacheMatrix. If the inverse has already
+## been solved it will return the inverse from the cache with the message "Getting cached data"
 cacheSolve <- function(x, ...) 
 {
   inv <- x$getInverse()
